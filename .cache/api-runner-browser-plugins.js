7e07fffdc5eef83e720ea1d18b9abbc2b7dca2ab
Grace Scheibe <gscheibe@iastate.edu>
2019-01-14 13:53:14 -0600

    changed avatars, bio, and readme

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/gracescheibe/git/personal-website/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/gracescheibe/git/personal-website/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
